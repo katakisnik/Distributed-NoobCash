c7028562f8099b5c403c451697debdd564112154
katakisnik <nikif31@hotmail.com>
2020-03-16 21:21:57 +0200

    added state,settings and begun block.py

---
+++ src/state.py
@@ -0,0 +1,33 @@
+#Global variables for each state of our application
+
+# list of validated blocks
+blockchain = []
+
+# list of valid transactions that are not yet in a block
+transactions = []
+
+# list of participants
+participants = {}
+
+# number of participants
+num_participants = -1
+
+#participant_id = 0 -> coordinator
+participant_id = -1
+
+#private and public key for each participant
+publickey = None
+privatekey = None
+
+# unspent transactions of each participant
+utxos = {}
+
+# validated utxos, untill final validated block
+valid_utxos = {}
+
+# pid of miner
+miner_pid = None
+
+# genesis block and utxos
+genesis_block = None
+genesis_utxos = []
