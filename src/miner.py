afe1c29ef61331e7b64779ec986f5fefa061518d
Dimitris Katsiros <dkatsiros@gmail.com>
2020-03-17 14:42:11 +0200

    Add transaction money calculation

---
+++ src/miner.py
@@ -0,0 +1,9 @@
+# Get a transaction send(x,(A,B))
+
+# Verify transaction came from A using public key
+
+# Check A has enough money to call transaction
+
+# If all checks succeed,
+
+# add block to state
--- src/transaction.py
+++ src/transaction.py
@@ -4,7 +4,7 @@ import copy
 class Transaction(object):
 
     #constructor
-    def __init__(self, sender, receiver, amount, id, inputs, signature):
+    def __init__(self, sender, receiver, amount, id, inputs=None, signature=None):
         self.sender = sender
         self.receiver = receiver
         self.amount = amount
@@ -20,18 +20,35 @@ class Transaction(object):
         if (self.sender == value.sender and self.receiver == value.receiver and self.amount == value.amount and self.id == value.id and self.inputs == value.inputs and self.signature == value.signature):
             return True
 
+    def sign(self):
+        pass
+        
+
     @staticmethod
     def create_transaction(receiver, amount):
         try:
+            # Sender creates this transaction, at that time state.publickey is his pk
             sender = state.publickey
-            sender_utxos = copy.deepcopy(state.utxos[sender]) 
+            sender_utxos = copy.deepcopy(state.utxos[sender])
             receiver_utxos = copy.deepcopy(state.utxos[receiver])
 
-            #id for each transaction that led to the money that are being transfered
+            # id for each transaction that led to the money that are being transfered
             inputs = []
             for tr in sender_utxos:
                 inputs.append(tr['id'])
-        
+
+            # available money of the sender
+            sender_money = [t_utxo['amount'] for t_utxo in sender_utxos if t_utxo['who'] == sender ]
+            # Check if the available money is enough
+            if sender_money < amount :
+                raise Exception(f'User with PublicKey({sender}) has not enough money')
+
+            # Create the transaction
+            t = Transaction(sender,receiver,amount,id=new_id,inputs=inputs)
+            t.sign()
+            # Each transactions have
+            # utxos[pubkey] = [{transaction_id, who, amount}]
+
         except Exception as e:
             print(e)
             return None
