dc43bf0fdb93741ed8823b98cde46890db10a02f
Dimitris Katsiros <dkatsiros@gmail.com>
2020-03-22 01:34:23 +0200

    Add python version as settings

--- src/trydjango/nbcsettings.py
+++ src/trydjango/nbcsettings.py
@@ -1,6 +1,11 @@
+"""All settings needed for nbc to be functional."""
+
+PYTHON_VERSION = 'python3.6'
+
 BLOCK_CAPACITY = 5
 
 DIFFICULTY = 3
 
 COORDINATOR_PORT = 8000
+
 CORDINATOR = f'http://127.0.0.1:{COORDINATOR_PORT}'
