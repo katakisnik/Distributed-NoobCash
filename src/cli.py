52b8fba82cb835d792e5a193e3c9784fe0d2b90e
katakisnik <nikif31@hotmail.com>
2020-03-22 15:09:05 +0200

    trying to fix the client

--- src/cli.py
+++ src/cli.py
@@ -21,6 +21,16 @@ api = f'{coordinatorhost}/create_coordinator/'
 if participants == 0:
     print('Hello i am a normal participator')
     api = f'{coordinatorhost}/create_participant'
+    try:
+        response = requests.post(api, {
+            'num_participants': participants,
+            'host': host
+        })
+        assert response.status_code == 200
+    except Exception as e:
+        print('failed')
+        exit(1)
+
 else:
     try:
         response = requests.post(api, {
