dda1631c71cd0313005fc79b9c5cd6c4cedb06ff
katakisnik <nikif31@hotmail.com>
2020-03-23 12:38:09 +0200

    added all the machines in update_machine.sh

--- update_machine.sh
+++ update_machine.sh
@@ -1,2 +1,5 @@
 #!/bin/env bash
 scp -r ../Distributed-NoobCash user@192.168.1.1:~/
+scp -r ../Distributed-NoobCash user@192.168.1.4:~/
+scp -r ../Distributed-NoobCash user@192.168.1.2:~/
+scp -r ../Distributed-NoobCash user@192.168.1.3:~/
