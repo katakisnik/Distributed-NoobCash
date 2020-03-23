bd6051f2d11e51ad9aa8edd6bb08261b7b34a005
katakisnik <nikif31@hotmail.com>
2020-03-23 13:57:35 +0200

    small fix in broadcast

--- src/trydjango/broadcast.py
+++ src/trydjango/broadcast.py
@@ -5,6 +5,9 @@ def broadcast(api, message):
     #we send the message as data for each other /host/api
 
     for participant in state.participants:
+        if participant['id'] == state.participant_id:
+            continue
+
         host = participant['host']
 
         req = requests.post(f'{host}/{api}/', message)
