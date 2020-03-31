e9d313560163d4bcd265976a292802c2f38b10be
katakisnik <nikif31@hotmail.com>
2020-03-31 16:56:24 +0300

    gtxm

---
+++ src/trydjango/mineprocess.py
@@ -0,0 +1,38 @@
+import sys
+from random import randint
+import datetime
+import json
+import requests
+
+from Crypto.Hash import SHA384
+
+if __name__ == '__main__':
+    """If we run main then start_mine should be initialized."""
+    start_mine(sys.argv[1], sys.argv[2], sys.argv[3])
+
+def start_mine(host, transactions, difficulty):
+    """Find nonce and send API back to the main process."""
+    transactions = transactions
+    b = {}
+    b['transactions'] = transactions
+    nonce = randint(0, 1000000)
+    # Create API
+    host = host
+    API = f'{host}/create_block/'
+    while True:
+        b['nonce'] = nonce
+        b['timestamp'] = str(datetime.datetime.now())
+        b_json = json.dumps(b, sort_keys=True)
+        sha = SHA384.new(b_json.encode()).hexdigest()
+        if sha.startswith('0' * difficulty):
+            # Send API to process that we forked from
+            block_details = {
+                'transactions': b['transactions'],
+                'nonce': b['nonce'],
+                'sha': sha,
+                'timestamp': b['timestamp']
+            }
+            requests.post(API, block_details)
+            exit(0)
+        # Continue until you compute nonce
+        nonce = randint(0, 1000000)
